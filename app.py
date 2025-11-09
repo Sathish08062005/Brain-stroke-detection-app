e6c29cf48cf8ceb57edeb1b0b1bd144297234265
Sathish kumar <sath7094@gmail.com>
2025-11-09 10:25:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 import os
 import gdown
 import tensorflow as tf
-import pil import image
+import PIL import image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 import matplotlib.pyplot as plt
